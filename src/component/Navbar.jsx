fb5faec90441d50490325fa6e17d540e6d780cf6
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2022-12-25 09:49:28 -0800

    Adding images

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -15,10 +15,17 @@ function BasicExample() {
         setScroll(false);
       }
     };
-  });
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActivelink(value);
+  };
 
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scroll ? "scroll" : ""}>
       <Container>
         <Navbar.Brand href="#home">
           <img src={""} alt="logo" />
@@ -28,20 +35,44 @@ function BasicExample() {
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
 
-            <Nav.Link href="#projects">projects</Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("porjects")}
+            >
+              projects
+            </Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#">
+              <a href="home">
                 <img src={""} alt=" " />
               </a>
-              <a href="#">
+              <a href="home">
                 <img src={""} alt=" " />
               </a>
-              <a href="#">
+              <a href="home">
                 <img src={""} alt=" " />
               </a>
             </div>
