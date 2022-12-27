c6a692cc273f47d6bb1cb78462ba969b644ff72f
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2022-12-27 11:01:20 -0800

    Compeleted  Banner component

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -1,6 +1,49 @@
+import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import "../App.css";
 
 export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [text, delta]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updateText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updateText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+    if (!isDeleting && updateText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updateText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -11,7 +54,7 @@ export const Banner = () => {
               {`Hi I'm a Frontend `}
               <span className="wrap" mg={5}>
                 <br />
-                Web developer.
+                {text}
               </span>
             </h1>
             <p>
@@ -20,9 +63,12 @@ export const Banner = () => {
               nesciunt minima ullam eveniet tenetur! Dolorem eveniet, minima
               corrupti quod sapiente omnis?
             </p>
-            <button onClick={() => console.log("connected")}>
-              Let's Connect
+            <button className="wwd" onClick={() => console.log("connected")}>
+              Let's Connect <ArrowRightCircle size={25} />
             </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header-Img" />
           </Col>
         </Row>
       </Container>
