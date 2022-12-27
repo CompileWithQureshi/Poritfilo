a1667e73aeddc3dc1c829d9c9903c834f25ec535
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2022-12-27 10:13:37 -0800

    Creating Banner component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './component/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Banner } from './component/Banner';
 
 const App = () => {
   return (
     <div >
       <Navbar />
+      <Banner />
     </div>
   )
 }
---
+++ src/component/Banner.jsx
@@ -0,0 +1,28 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export const Banner = () => {
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-item-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi I'm a Frontend `}
+              <span className="wrap" mg={5}>
+                <br />
+                Web developer.
+              </span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Molestiae vero commodi at unde. Ipsa sint tempore quod eum,
+              nesciunt minima ullam eveniet tenetur! Dolorem eveniet, minima
+              corrupti quod sapiente omnis?
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
