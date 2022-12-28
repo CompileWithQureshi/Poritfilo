c48329be2a79104503e005cd0e185ffb1ac9b1b6
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2022-12-28 02:29:20 -0800

    Creating Skill component

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -7,7 +7,12 @@ import "../App.css";
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const toRotate = [
+    "Web Developer",
+    "Web Designer",
+    "UI/UX Designer",
+    "Backend Developer",
+  ];
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const period = 2000;
@@ -32,7 +37,7 @@ export const Banner = () => {
     setText(updateText);
 
     if (isDeleting) {
-      setDelta((prevDelta) => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 5);
     }
     if (!isDeleting && updateText === fullText) {
       setIsDeleting(true);
@@ -51,7 +56,7 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hi I'm a Frontend `}
+              {`Hi I'm a `}
               <span className="wrap" mg={5}>
                 <br />
                 {text}
